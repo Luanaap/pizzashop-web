597dc1b593d95cd90a6cf2fb4f8cf0e763d39b0c
Luanaap <57630311+Luanaap@users.noreply.github.com>
2025-07-16 19:21:03 -0300

    feat: add application routes and dashboard

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { RouterProvider } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import './index.css';
+import { router } from './routes';
+import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/react-query';
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey='pizza-shop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate="%s | pizza.shop"/>
+        <Toaster richColors/>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}/>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
+  )
+}
